98b11168e5b19559186614c970d00b01f902596f
dongMin <vvslc77@naver.com>
2023-12-17 15:55:30 +0900

    flip 구현 및 모바일 스크롤 방지

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import MoveBox from './components/MoveBox.vue'
+import MoveBox from '@/components/MoveBox.vue'
 </script>
 
 <template>
@@ -15,70 +14,7 @@ import MoveBox from './components/MoveBox.vue'
     draggable="false"
   >
     <div class="title svelte-1anl9sr" style="text-align: center">Task</div>
-    <MoveBox :list="['red', 'orange', 'green', 'blue']" :width="'width: 400px'" :height="'height: 60px'" :id="'above'" />
-    <MoveBox :list="['indigo', 'purple']" :width="'width: 200px'" :height="'height: 120px'" :id="'under'"/>
+    <MoveBox :list="['red', 'orange', 'green', 'blue']" :width="'width: 350px'" :height="'height: 60px'" :id="'above'" />
+    <MoveBox :list="['indigo', 'purple']" :width="'width: 200px'" :height="'height: 120px'" :id="'under'"/>    
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/components/MoveBox.vue
+++ src/components/MoveBox.vue
@@ -10,6 +10,9 @@ const props = defineProps<{
 
 const mouseFlag = ref(false);
 const initX = ref(0);
+const flipX = ref(0);
+const flipTime = ref(0);
+const speedArr = ref([{}]);
 const translate = ref(0);
 const opacity = ref(100);
 const transition = ref('');
@@ -19,7 +22,7 @@ const interval = ref();
 const colorList = props.list;
 const idx = ref(0);
 const status = ref('AUTO TRANSITION');
-
+const fullWidth = ref(0);
 const translateBind = computed(() => `translate3d(${translate.value}px, 0px, 0px)`);
 const opacityBind = computed(() => `${opacity.value}%`);
 
@@ -30,7 +33,7 @@ document.addEventListener('mousemove', function(event: MouseEvent) {
 })
 
 // pc 마우스 up 이벤트
-document.addEventListener('mouseup', function(event: MouseEvent) {
+document.addEventListener('mouseup', function() {
   boxEnd();
 })
 
@@ -60,6 +63,7 @@ function afterColor() {
 function alertfunction() {
   if (!mouseMoveFlag.value) {
     alert('CLICK : ' + colorList[idx.value]);
+    status.value = 'CLICK : ' + colorList[idx.value]
   }
 }
 
@@ -67,7 +71,7 @@ function alertfunction() {
 function autoTransition() {
   transitionColor.value = afterColor()
   transition.value = '0.3s ease 0s';
-  translate.value = 400;
+  translate.value = fullWidth.value;
   idx.value = idx.value === colorList.length - 1 ? 0 : idx.value + 1;
   status.value = 'AUTO TRANSITION';
   opacity.value = 0;
@@ -75,6 +79,7 @@ function autoTransition() {
 
 onMounted(() => {
   interval.value = setInterval(autoTransition, 3000);
+  fullWidth.value = document.getElementById(props.id)!.offsetWidth ?? 0;
 })
 
 // pc 마우스 start
@@ -91,13 +96,13 @@ function mouseEnd() {
 // 모바일 터치 move
 function touchmove(event: TouchEvent) {
   const x = event.touches[0].clientX;
-  boxMove(x);
+  boxMove(x);  
 }
 
 // 모바일 터치 start
 function touchstart(event: TouchEvent) {
   const x = event.touches[0].clientX;
-  boxStart(x);
+  boxStart(x);  
 }
 
 // 모바일 터치 end
@@ -110,34 +115,48 @@ function touchend() {
 function boxStart(x: number) {
   translate.value = 0;
   initX.value = x;
+  flipX.value = x;
+  flipTime.value = new Date().getTime();
   transition.value = '0s';
   mouseFlag.value = true;
   clearInterval(interval.value);
 }
 
+// slide 시 TO, FLIP 구분
+function slideType(type: string) {
+  translate.value = translate.value < 0 ? -fullWidth.value : fullWidth.value;
+  idx.value = idx.value === colorList.length - 1 ? 0 : idx.value + 1;
+  status.value = translate.value < 0 ? `${type} LEFT` : `${type} RIGHT`;
+  opacity.value = 0;
+}
+
 // 상자 클릭 end 이벤트
 function boxEnd() {
-  const width = document.getElementById(props.id)!.offsetWidth;
+  const time = new Date().getTime();  
   transition.value = '0.3s ease 0s';
-  if (mouseFlag.value) {
-    if (toPositiveNumber(translate.value) >= width / 2) {
-      translate.value = translate.value < 0 ? -400 : 400;
-      idx.value = idx.value === colorList.length - 1 ? 0 : idx.value + 1;
-      status.value = translate.value < 0 ? 'TO LEFT' : 'TO RIGHT';
-      opacity.value = 0;
+  if (mouseFlag.value) {        
+    const flip = speedArr.value.filter((e:any) => time - 10 <= e.time ).filter((e:any) => e.value >= 0.1).length;
+    if (flip > 0) {
+      slideType('FLIP')         
     } else {
-      status.value = 'CANCEL';
-      translate.value = 0;
-      opacity.value = 100;
+      if (toPositiveNumber(translate.value) >= fullWidth.value / 2) {
+        slideType('TO')
+      } else {
+        status.value = 'CANCEL';
+        translate.value = 0;
+        opacity.value = 100;
+      }
     }
   }
 
-  mouseFlag.value = false
+  speedArr.value = [];
+  mouseFlag.value = false;
 }
 
 // 상자 클릭 움직임 이벤트
-function boxMove(x: number) {
-  if (mouseFlag.value) {
+function boxMove(x: number) {  
+  if (mouseFlag.value) { 
+    flip(x);
     const distance = x - initX.value;
     transitionColor.value = afterColor();
     if (status.value.startsWith('SWIPE')) {
@@ -170,10 +189,22 @@ function transitionStart() {
     transitionColor.value = colorList[idx.value];
   }
 }
+
+// 플립
+function flip(x: number) {
+  const time = new Date().getTime();
+  const arrDistance = toPositiveNumber(x - flipX.value);
+  const arrTime = time - flipTime.value;
+  const arrSpeed = arrDistance / arrTime;  
+  speedArr.value.push({time: time, value: arrSpeed});
+  flipTime.value = time;
+  flipX.value = x;    
+}
+
 </script>
 
 <template>
-    <div class="wrap-card svelte-1anl9sr" :style="props.width">
+    <div class="wrap-card svelte-1anl9sr" :style="props.width">          
       <div class="blackText">{{ status }}</div>
       <div
         class="card-wrapper svelte-1oijsep"
@@ -205,8 +236,8 @@ function transitionStart() {
         </div>
         <div class="gesture-recognizer svelte-p8tcev" style="left: 0px; top: 0px"></div>
       </div>
-    </div>
     <div class="grayText" draggable="false">{{ colorList }}</div>
+    </div>
 </template>
 
 <style scoped>
